5cb5e1c9004293376de67a26d21e5c2e8e800abf
Rakhim9830 <106856795+Rakhim9830@users.noreply.github.com>
2022-10-14 20:27:28 +0600

    Initial commit

---
+++ src/Boss.java
@@ -0,0 +1,29 @@
+public class Boss {
+    private int bossHealth;
+    private int bossDamage;
+    private String typeOfDefence;
+
+    public int getBossDamage() {
+        return this.bossDamage;
+    }
+
+    public void setBossDamage(int bossDamage) {
+        this.bossDamage = bossDamage;
+    }
+
+    public int getBossHealth() {
+        return this.bossHealth;
+    }
+
+    public void setBossHealth(int bossHealth) {
+        this.bossHealth = bossHealth;
+    }
+
+    public String getTypeOfDefence() {
+        return typeOfDefence;
+    }
+
+    public void setTypeOfDefence(String typeOfDefence) {
+        this.typeOfDefence = typeOfDefence;
+    }
+}
